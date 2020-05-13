45b1f556ea111dbc2dc4800e5098612e90e8b253
lrminer <loganrobertminer@gmail.com>
2020-05-13 07:17:23 -0400

    test
    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import LandingPage from "./pages/LandingPage";
-
-
-
+ 
+ 
 function App() {
   return (
     <div className="App">
