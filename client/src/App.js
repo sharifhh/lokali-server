80fc1cba02ec4080e18a730bbb9e0afe89f59ac6
lrminer <60660824+lrminer@users.noreply.github.com>
2020-05-13 06:14:50 -0400

    init

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,13 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import LandingPage from "./pages/LandingPage";
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LandingPage />
     </div>
   );
 }
---
+++ client/src/pages/LandingPage.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+const LandingPage = (props) => {
+  return (
+    <>
+      <ul class="nav d-flex justify-content-end">
+        <li class="nav-item pr-3">
+          <button type="button" class="btn btn-primary">
+            Login
+          </button>
+        </li>
+        <li class="nav-item">
+          <button type="button" class="btn btn-primary">
+            Register
+          </button>
+        </li>
+      </ul>
+      <div>
+        <h1>Heading</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione totam
+          rerum neque labore veniam enim incidunt tempora non laudantium
+          necessitatibus? Vitae pariatur harum eveniet hic fugiat, ad magnam
+          dolores consequuntur!
+        </p>
+      </div>
+      <div>
+        <h1>Heading</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione totam
+          rerum neque labore veniam enim incidunt tempora non laudantium
+          necessitatibus? Vitae pariatur harum eveniet hic fugiat, ad magnam
+          dolores consequuntur!
+        </p>
+      </div>
+      <div className="d-flex">
+        <div>
+          <h1>Heading</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+            totam rerum neque labore veniam enim incidunt tempora non laudantium
+            necessitatibus? Vitae pariatur harum eveniet hic fugiat, ad magnam
+            dolores consequuntur!
+          </p>
+        </div>
+        <div>
+          <h1>Heading</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+            totam rerum neque labore veniam enim incidunt tempora non laudantium
+            necessitatibus? Vitae pariatur harum eveniet hic fugiat, ad magnam
+            dolores consequuntur!
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LandingPage;
