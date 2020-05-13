110effbf45f5c2677a460adc5281503208318208
lrminer <loganrobertminer@gmail.com>
2020-05-13 08:05:03 -0400

    Login page initialized

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "./fontawesome.min.css";
+import Login from "./pages/Login";
 function App() {
   return (
     <div className="App">
-      
+      <Login />
     </div>
   );
 }
