bc9221251eb37e706ab8a4019c1fb7b15dfc29a2
lrminer <loganrobertminer@gmail.com>
2020-05-14 07:16:09 -0400

    initialized add offer page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+import AddOffering from './pages/AddOffering'
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <AddOffering />
     </div>
   );
 }
