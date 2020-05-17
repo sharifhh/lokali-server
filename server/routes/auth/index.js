6fa9bf27857df3c18fc3c963079e487fa97cb0f7
Danny Borisov <dannyboris1993@gmail.com>
2020-05-17 22:29:44 +0300

    added redirect on signup success. added password hash on signup

--- server/routes/auth/index.js
+++ server/routes/auth/index.js
@@ -1,15 +1,18 @@
 const db = require('../../models');
 const connectToDB = require('../../dbConnection')
+const  bcrypt = require('bcrypt')
 
 module.exports = (app) => {
     
     app.post('/auth/signup',  async (req,res)=>{
         let newUser = req.body
+        let hashPass =  bcrypt.hashSync(newUser.password,10)
+        newUser.password = hashPass
+        console.log(newUser)
         await connectToDB()
         try{
           await db.User.create(newUser, (err,doc) => {
               let dbRes = err ? err : doc
-              console.log('saul goodman!')
               res.json(dbRes)
           })
         }
