fd8443e169291f9733f7e8b8c0beac448681aed7
Danny Borisov <dannyboris1993@gmail.com>
2020-05-18 03:14:29 +0300

    refactored register, added profile modal

--- client/components/Register/Register.jsx
+++ client/components/Register/Register.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext, useEffect } from 'react';
 import styles from './Register.module.css'
-import { LOGIN_TYPE, SIGNUP_TYPE, FORM_SIGNUP_TITLE, FORM_LOGIN_TITLE, FORM_SIGNUP_SUBTITLE, FORM_LOGIN_SUBTITLE, USER_EXISTS_ERR_CODE } from '../../constants';
+import { LOGIN_TYPE, SIGNUP_TYPE, USER_EXISTS_ERR_CODE, FORM_LOGIN_INFO, FORM_SIGNUP_INFO, ALL_FIELDS_REQUIRED_ERR, WRONG_INFO_ERR, USER_EXISTS_ERR} from '../../constants';
 import { capitalize } from '../../utils';
 import { AuthContext } from '../../context/AuthContext';
 import Router from 'next/router';
@@ -10,41 +10,30 @@ const Register = () => {
     const {login, signup, setCurrLoggedUser} = useContext(AuthContext)
     //Register State
     const [form,setForm] = useState({name:'',password:'', email:'', createSession:false})
-    const [formType, setFormType] = useState(SIGNUP_TYPE)
-    const [title, setTitle] = useState(FORM_SIGNUP_TITLE)
-    const [subTitle, setSubTitle] = useState(FORM_SIGNUP_SUBTITLE)
-    const [showErrMsg,setShowErrMsg] = useState({msg:'All Fields are required', show:false, loading:false})
+    const [formInfo, setformInfo] = useState(FORM_SIGNUP_INFO)
+    const [showErrMsg,setShowErrMsg] = useState({msg:ALL_FIELDS_REQUIRED_ERR , show:false, loading:false})
    
-    useEffect(()=> setShowErrMsg(false),[formType ,form]) //useEffect reset err msg when switch between forms or when user types
+    useEffect(()=> setShowErrMsg(false),[form]) //useEffect reset err msg when switch between forms or when user types
     //On type switch (LOGIN --> SIGNUP and vice versa) TODO: Consider using reducerer
     const handleFormType = () =>{
-        let switchFormType = formType === SIGNUP_TYPE ? LOGIN_TYPE : SIGNUP_TYPE
-        let switchTitle = formType === SIGNUP_TYPE ? FORM_LOGIN_TITLE : FORM_SIGNUP_TITLE
-        let switchSubTitle = formType === SIGNUP_TYPE ? FORM_LOGIN_SUBTITLE : FORM_SIGNUP_SUBTITLE
-        setFormType(switchFormType)
-        setTitle(switchTitle)
-        setSubTitle(switchSubTitle)
+        let nextFormInfo = formInfo.type === SIGNUP_TYPE ? {...FORM_LOGIN_INFO} : {...FORM_SIGNUP_INFO}
+        setformInfo(nextFormInfo)
     }
-    //Handle input change and submit
-    const handleNameChange = e => setForm({...form, name:e.target.value})
-    const handleEmailChange = e => setForm({...form, email:e.target.value})
-    const handlePasswordChange = e => setForm({...form, password:e.target.value})
-    const handleCreateSession = e =>{ setForm({...form, createSession:e.target.checked})}
-    
-    //SUBMIT
+
+    const handleChange  = e => setForm({...form, [e.target.id]:e.target.value })    
     const handleSubmit = async () =>{
-       //Front end validation 
+        //Front end validation 
        let isValid = validateForm()
-       if(!isValid) setShowErrMsg({show:true, msg:'All Fields are required'})
+       if(!isValid) setShowErrMsg({show:true, msg:ALL_FIELDS_REQUIRED_ERR})
 
        else{
-           let res =  formType === LOGIN_TYPE ? await login(form) : await signup(form)
+           let res =  formInfo.type === LOGIN_TYPE ? await login(form) : await signup(form)
            if(!res) {
-            setShowErrMsg({show:true,msg: 'Email/Password are incorrect, Please try again'})
+            setShowErrMsg({show:true,msg:WRONG_INFO_ERR})
             return
            }
            if(res.code === USER_EXISTS_ERR_CODE) {
-                setShowErrMsg({show:true,msg:'User Exists'})
+                setShowErrMsg({show:true,msg:USER_EXISTS_ERR})
                 return
            }
            console.log(res)
@@ -55,35 +44,56 @@ const Register = () => {
 
     const validateForm = () =>{
         let formToValidation = Object.assign({}, {...form})        
-        if(formType === LOGIN_TYPE) delete formToValidation.name
+        if(formInfo.type === LOGIN_TYPE) delete formToValidation.name
         delete formToValidation.createSession
         let isValid = Object.values(formToValidation).every(field=>field)
         return isValid
 
     }
+    const renderInputs = () =>{
+        let formToRender = Object.assign({},{...form})
+        delete formToRender.createSession
+        if(formInfo.type === LOGIN_TYPE) delete formToRender.name
+        return  Object.keys(formToRender).map(field=>{  
 
+            return (
+                <input 
+                type="text"
+                id={field}
+                value={form[field]} 
+                placeholder={`Enter ${capitalize(field)}`}
+                onChange={handleChange}/>
+            )
+        })
+    
+    }
     return ( 
         <div className={styles.container}>
             <div className={styles.register}>
-            <h1>{title}</h1>
-            <p>{subTitle}</p>
-            { formType === SIGNUP_TYPE ? <input onChange={handleNameChange} value={form.name} placeholder="Enter Name" type="text"/> : null}
-            <input onChange={handleEmailChange} value={form.email} placeholder="Enter Email" type="text"/>
-            <input onChange={handlePasswordChange} value={form.password} placeholder="Enter Password" type="text"/>
+            <Header title={formInfo.title} subtitle={formInfo.subtitle}/>
+            {renderInputs()}
             <div className={styles.lowerSect}>
                 <div className={styles.remAuth}>
-                <input onChange={handleCreateSession} checked={form.createSession} type="checkbox"/>
+                <input onChange={handleChange} checked={form.createSession} type="checkbox"/>
                 <label>Remember Me</label>
                 </div>
-                <button onClick={handleSubmit}>{capitalize(formType)}</button>
+                <button onClick={handleSubmit}>{capitalize(formInfo.type)}</button>
             </div>
                 {showErrMsg.show ?  <span className={styles.errMsg}>{showErrMsg.msg}</span> : null}
                 <span  onClick={handleFormType} className={styles.redirectSpan}>
-                    {formType === SIGNUP_TYPE ? 'Already have an account? Login!' : 'New? Signup'}
+                    {formInfo.type === SIGNUP_TYPE ? 'Already have an account? Login!' : 'New? Signup'}
                 </span>
             </div>
         </div>
         
+     );
+}
+export const Header = ({title,subtitle}) => {
+    return ( 
+        <div className="header">
+            <h1>{title}</h1>
+            <p>{subtitle}</p>
+        </div>
      );
 }
  
--- client/pages/profile.jsx
+++ client/pages/profile.jsx
@@ -1,43 +1,25 @@
 import React from 'react';
-import {Modal} from '@material-ui/core'
 import styles from './styles/Profile.module.css'
+import {Modal} from '@material-ui/core'
+
 const Profile = () => {
     return ( 
-            <Modal open={true}>
-                <div className={`${styles.container} flex`}>
-                  <div className={`${styles.innerContainer} `}>
-                    <div className={`flex column ${styles.sidebar}`}>
-                            <SideBarItem text="Click to add your photo"/>
-                            <SideBarItem text="Your score and Budges"/>
+                <div className={`${styles.profileModalContainer} modal space-evenly`}>
+                    <span className={`${styles.title} modal`}>My Profile</span>
+                    <div className={`${styles.innerContainer} flex `}>
+                        <div className={`${styles.sidebar} flex column space-around`}>
+                            <div className={styles.sidebarItem}><h1>ITEM</h1></div>
+                            <div className={styles.sidebarItem}><h1>ITEM</h1></div>
                         </div>
-                        <div className="details">
-                            <Input label="Name"/>
-                            <Input label="Name"/>
-                            <Input label="Name"/>
-                            <Input label="Name"/>
-                        </div>
-                  </div>
+                        <div className={styles.profileForm}><h1>fds</h1></div>
+                    </div>
+                    <div className={`flex self-right ${styles.btnContainer}`} >
+                        <button>Save</button>
+                        <button>Cancel</button>
+                    </div>
                 </div>
-            </Modal>
      );
 }
 
-export const Input = ({label}) => {
-    return ( 
-        <div class={styles.input}>
-            <label>{label}</label>
-            <input type="text"/>
-        </div>
-     );
-}
 
-export const SideBarItem = ({text}) => {
-    return ( 
-        <div className={styles.sidebaritem}>
-            <span>{text}</span>
-        </div>
-     );
-}
- 
- 
 export default Profile;
