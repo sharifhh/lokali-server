b1491075f528f2a4f9d791e14a10244e3ed82e9c
Logan Miner <loganrobertminer@gmail.com>
2020-05-22 13:26:44 -0400

    Update ModalButton.jsx

--- client/components/PopupModalDialog/ModalButton.jsx
+++ client/components/PopupModalDialog/ModalButton.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const ModalButton = ({  children, onClick, bg="", color=""}) => {
+const ModalButton = ({  text, onClick, bg="", color=""}) => {
     
-    return <button style={{background:bg, color:color}} className="button" onClick={onClick}>{children}</button>
+    return <button style={{background:bg, color:color}} className="button" onClick={onClick}>{text}</button>
     }
 
 export default ModalButton;
