adae4c919c5df29e54dff16bad51e975bc9f0431
Logan Miner <loganrobertminer@gmail.com>
2020-05-22 12:25:19 -0400

    Update login.jsx

--- client/pages/login.jsx
+++ client/pages/login.jsx
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-import React, { useState, useContext } from 'react';
-import {Button, TextField} from '@material-ui/core'
-import AuthContextProvider, { AuthContext } from '../context/AuthContext'
-
-const Login = () => {
-    const {user, setUser, login}  = useContext(AuthContext)
-    const handleNameChange = e => setUser({...user, name:e.target.value})
-    const handlePasswordChange = e => setUser({...user, password:e.target.value})
-    const handleSubmit = async e =>{
-        let res = await login()
-        console.log(res)
-    }
-        return ( 
-        <AuthContextProvider>
-        <div className="form">
-        <h2>Login</h2>
-        <TextField onChange={handleNameChange} defaultValue={user.name}  label="Username"/>
-        <TextField onChange={handlePasswordChange} defaultValue={user.password}  label="Password"/>
-         <Button onClick={handleSubmit}  variant="contained" color="primary">Submit</Button>
-         <div>
-
-         </div>
-         <style jsx>{`
-           .form{
-               border:1px solid rgba(144,144,144,.4);
-               display:flex;
-               flex-direction:column;
-               width:75%;
-               height:50vh;
-               padding:20px;
-               justify-content:space-evenly;
-           }
-      `}</style>
-
-        </div>
-    </AuthContextProvider>
-=======
 import React, { useState, useEffect, useContext } from 'react';
 import ModalOuterContainer from '../components/PopupModalDialog/ModalOuterContainer';
 import ModalTitle from '../components/PopupModalDialog/ModalTitle';
@@ -86,7 +48,6 @@ const Login = () => {
                     </ModalButtonContainer>
             </ModalOuterContainer>
         </div>
->>>>>>> master
      );
 }
  
